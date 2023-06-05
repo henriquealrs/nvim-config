ddd7e96bace2ab8dfefead33ef51c7e1631e05fe
Henrique Lustosa <henriquealrs@gmail.com>
2023-06-05 11:45:45 -0300

    Att toggle term and .gitignore

--- lua/hlustosa/packer.lua
+++ lua/hlustosa/packer.lua
@@ -8,7 +8,6 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
   use('navarasu/onedark.nvim', {
@@ -38,39 +37,25 @@ return require('packer').startup(function(use)
 		  require('Comment').setup()
 	  end
   }
-  -- use('neovim/nvim-lspconfig') 
-  -- use('hrsh7th/cmp-nvim-lsp')
-  -- use('hrsh7th/cmp-buffer')
-  -- use('hrsh7th/cmp-path')
-  -- use('hrsh7th/cmp-cmdline')
-  -- use('hrsh7th/nvim-cmp')
-  -- use('neoclide/coc.nvim', {branch = 'release'})
-  -- use {
-	 --  "williamboman/mason.nvim",
-	 --  run = ":MasonUpdate" -- :MasonUpdate updates registry contents
-  -- }
 
-  -- use('ycm-core/YouCompleteMe')
-  -- use('grailbio/bazel-compilation-database')
-
-
-use('williamboman/mason.nvim', { vim.cmd, 'MasonUpdate'})
+  use('williamboman/mason.nvim', { vim.cmd, 'MasonUpdate'})
 
   use {
 	  'VonHeikemen/lsp-zero.nvim',
 	  branch = 'v2.x',
 	  requires = {
 		  -- LSP Support
-	  {'neovim/nvim-lspconfig'},             -- Required
-	  {'williamboman/mason-lspconfig.nvim'}, -- Optional
-	  -- Autocompletion
-	  {'hrsh7th/nvim-cmp'},     -- Required
-	  {'hrsh7th/cmp-nvim-lsp'}, -- Required
-	  {'L3MON4D3/LuaSnip'},     -- Required
+		  {'neovim/nvim-lspconfig'},             -- Required
+		  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},     -- Required
+		  {'hrsh7th/cmp-nvim-lsp'}, -- Required
+		  {'L3MON4D3/LuaSnip'},     -- Required
+	  }
   }
-}
 
+  use {"akinsho/toggleterm.nvim", tag = '*', config = function()
+	  require("toggleterm").setup()
+  end}
 
-
--- use('ludovicchabant/vim-gutentags')
 end)
