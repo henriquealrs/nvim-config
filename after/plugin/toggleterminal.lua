7e2fef1ac59cfb3726b3f78a6038bc8df30100b4
Henrique Ribeiro <henrique.ribeiro@stellantis.com>
2025-04-29 08:48:37 -0300

    Tweak toggleterminal

--- after/plugin/toggleterminal.lua
+++ after/plugin/toggleterminal.lua
@@ -1,1 +1,7 @@
-vim.keymap.set('n', '<leader>t', '<Cmd>exe v:count1 . "ToggleTerm"<CR>')
+require("toggleterm").setup{
+    on_open = function(t)
+        vim.cmd(":set rnu")
+    end,
+}
+
+vim.keymap.set('n', '<leader>t', '<Cmd>exe v:count1 . "ToggleTerm direction=float"<CR>')
