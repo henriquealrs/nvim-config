f38d5f97d7d8ee178531777464a17da96752f7c4
Henrique Lustosa <henriquealrs@gmail.com>
2025-05-10 14:54:21 -0300

    ToggleTerm: Change default shell to fish

--- after/plugin/toggleterminal.lua
+++ after/plugin/toggleterminal.lua
@@ -2,6 +2,7 @@ require("toggleterm").setup{
     on_open = function(t)
         vim.cmd(":set rnu")
     end,
+    shell = "fish"
 }
 
 vim.keymap.set('n', '<leader>t', '<Cmd>exe v:count1 . "ToggleTerm direction=float"<CR>')
