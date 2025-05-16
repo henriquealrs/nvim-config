335588a470f5a52a39ba5e120891cac55fd94128
Henrique Ribeiro <henrique.ribeiro@stellantis.com>
2025-05-16 14:31:58 -0300

    Add JqPlayground

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -251,13 +251,16 @@ require("lazy").setup({
             },
         },
 
-    -- import your plugins
-    { import = "plugins" },
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "rose-pine" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+        {
+            "yochem/jq-playground.nvim",
+        },
+        -- import your plugins
+        { import = "plugins" },
+    },
+    -- Configure any other settings here. See the documentation for more details.
+    -- colorscheme that will be used when installing plugins.
+    install = { colorscheme = { "rose-pine" } },
+    -- automatically check for plugin updates
+    checker = { enabled = true },
 })
 
