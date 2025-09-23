d5f2b9a2d4df41c433f182f4c2f910b5785b29ef
Henrique Lustosa <henriquealrs@gmail.com>
2025-09-23 08:59:47 -0300

    WIP

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,75 +1,72 @@
 -- NOTE: to make any of this work you need a language server.
 -- If you don't know what that is, watch this 5 min video:
 -- https://www.youtube.com/watch?v=LaS32vctfOY
+require('mason').setup()
+require('mason-lspconfig').setup({
+  ensure_installed = { 'lua_ls', 'clangd', 'pyright' },
+  automatic_enable = true,
+})
 
 -- Reserve a space in the gutter
 vim.opt.signcolumn = 'yes'
 
-require("neodev").setup({})  -- must be called *before* lua_ls setup
+require("neodev").setup({})
 
--- Add cmp_nvim_lsp capabilities settings to lspconfig
--- This should be executed before you configure any language server
-local lspconfig_defaults = require('lspconfig').util.default_config
-lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-    'force',
-    lspconfig_defaults.capabilities,
-    require('cmp_nvim_lsp').default_capabilities()
-)
+-- Add this line near the top (before vim.lsp.config calls):
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+capabilities.offsetEncoding = { "utf-16" }
 
--- This is where you enable features that only work
--- if there is a language server active in the file
+-- Define/override server configs (data only)
+vim.lsp.config('lua_ls', {
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = { version = "LuaJIT" },
+      diagnostics = { globals = { "vim" } },
+      workspace = { library = vim.api.nvim_get_runtime_file("", true), checkThirdParty = false },
+      telemetry = { enable = false },
+    },
+  },
+})
+
+vim.lsp.config('clangd', {
+  capabilities = capabilities,
+  -- example tweaks:
+  -- cmd = { "clangd", "--background-index" },
+  -- filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+})
+
+vim.lsp.config('gleam', { capabilities = capabilities })
+
+vim.lsp.config('pyright', {capabilities = capabilities})
+
+
+-- Auto-attach keymaps etc.
 vim.api.nvim_create_autocmd('LspAttach', {
     desc = 'LSP actions',
     callback = function(event)
-        local opts = {buffer = event.buf}
-
-        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-        vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+        vim.bo[event.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+        local opts = { buffer = event.buf }
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
+        vim.keymap.set({ 'n', 'x' }, '<F3>', function() vim.lsp.buf.format({ async = true }) end, opts)
+        vim.keymap.set('n', '<F4>', vim.lsp.buf.code_action, opts)
     end,
 })
 
--- You'll find a list of language servers here:
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
--- These are example language servers. 
-require('lspconfig').gleam.setup({})
--- require('lspconfig').ocamllsp({}) -- ocamllsp.setup({})
 
-require('lspconfig').lua_ls.setup {
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT for Neovim)
-        version = "LuaJIT",
-      },
-      diagnostics = {
-        -- Recognize `vim` global
-        globals = { "vim" },
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false,  -- disables telemetry notice
-      },
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-}
+-- Finally, enable by server-name; this activates them for their filetypes
+-- vim.lsp.enable({ 'lua_ls', 'clangd', 'gleam', 'pyright' })
 
 
 local cmp = require('cmp')
 local luasnip = require('luasnip')
-
--- Optional: Load friendly-snippets (VSCode-style) if you're using them
 require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
@@ -91,47 +88,7 @@ cmp.setup({
     }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-    }, {
-        { name = 'buffer' },
-    }),
-})
-
-
--- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
--- Set configuration for specific filetype.
---[[ cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-      { name = 'git' },
-    }, {
-      { name = 'buffer' },
+        -- { name = 'luasnip' },
+        -- { name = 'buffer' },
     })
- })
- require("cmp_git").setup() ]]-- 
-
--- -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
--- cmp.setup.cmdline({ '/', '?' }, {
---     mapping = cmp.mapping.preset.cmdline(),
---     sources = {
---         { name = 'buffer' }
---     }
--- })
---
--- -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
--- cmp.setup.cmdline(':', {
---     mapping = cmp.mapping.preset.cmdline(),
---     sources = cmp.config.sources({
---         { name = 'path' }
---     }, {
---             { name = 'cmdline' }
---         }),
---     matching = { disallow_symbol_nonprefix_matching = true }
--- })
---
--- -- Set up lspconfig.
--- local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
--- require('lspconfig').clangd.setup {
---     capabilities = capabilities
--- }
-
+})
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -53,12 +53,6 @@ require("lazy").setup({
             }
         },
         {
-            'mason-org/mason.nvim',
-            lazy = false,
-            opts = {},
-            version = "1.11.0"
-        },
-        {
             "Badhi/nvim-treesitter-cpp-tools",
             dependencies = { "nvim-treesitter/nvim-treesitter" },
             -- Optional: Configuration
@@ -115,43 +109,30 @@ require("lazy").setup({
         {
             'hrsh7th/nvim-cmp',
             event = 'InsertEnter',
-            config = function()
-                local cmp = require('cmp')
-
-                cmp.setup({
-                    sources = {
-                        { name = 'nvim_lsp' },
-                    },
-                    mapping = cmp.mapping.preset.insert({
-                        ['<C-Space>'] = cmp.mapping.complete(),
-                        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-                        ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                    }),
-                    snippet = {
-                        expand = function(args)
-                            vim.snippet.expand(args.body)
-                        end,
-                    },
-                })
-            end,
             dependencies = {
-                "L3MON4D3/LuaSnip",
-                "saadparwaiz1/cmp_luasnip",
-                "rafamadriz/friendly-snippets", -- optional, recommended
+                'hrsh7th/cmp-nvim-lsp',
+                'L3MON4D3/LuaSnip',
+                'saadparwaiz1/cmp_luasnip',
+                'rafamadriz/friendly-snippets',
             },
         },
 
+
         -- LSP
 
-        { 'hrsh7th/cmp-nvim-lsp' },
-        { 'hrsh7th/cmp-buffer' },
-        { 'hrsh7th/cmp-path' },
-        { 'hrsh7th/cmp-cmdline' },
-        { 'hrsh7th/nvim-cmp' },
-        { 'hrsh7th/cmp-vsnip' },
-        { 'hrsh7th/vim-vsnip' },
+        -- { 'hrsh7th/cmp-buffer' },
+        -- { 'hrsh7th/cmp-path' },
+        -- { 'hrsh7th/cmp-cmdline' },
 
-        { 'mason-org/mason-lspconfig.nvim', version = "1.32.0" },
+        {
+            'mason-org/mason.nvim',
+            lazy = false,
+            opts = {},
+        },
+        { 'mason-org/mason-lspconfig.nvim', version = "*",
+            -- config = function()
+            -- end,
+        },
         {
             'neovim/nvim-lspconfig',
             lazy = false,
@@ -164,49 +145,6 @@ require("lazy").setup({
                 -- This will avoid an annoying layout shift in the screen
                 vim.opt.signcolumn = 'yes'
             end,
-            config = function()
-                local lsp_defaults = require('lspconfig').util.default_config
-
-                -- Add cmp_nvim_lsp capabilities settings to lspconfig
-                -- This should be executed before you configure any language server
-                lsp_defaults.capabilities = vim.tbl_deep_extend(
-                    'force',
-                    lsp_defaults.capabilities,
-                    require('cmp_nvim_lsp').default_capabilities()
-                )
-
-                -- LspAttach is where you enable features that only work
-                -- if there is a language server active in the file
-                vim.api.nvim_create_autocmd('LspAttach', {
-                    desc = 'LSP actions',
-                    callback = function(event)
-                        local opts = { buffer = event.buf }
-
-                        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-                        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-                        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-                        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-                        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-                        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-                        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-                        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-                        vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-                        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-                    end,
-                })
-
-                require('mason-lspconfig').setup({
-                    ensure_installed = { "clangd", "lua_ls" },
-                    automatic_installation = true,
-                    handlers = {
-                        -- this first function is the "default handler"
-                        -- it applies to every language server without a "custom handler"
-                        function(server_name)
-                            require('lspconfig')[server_name].setup({})
-                        end,
-                    }
-                })
-            end
         },
         {
             "nvim-treesitter/nvim-treesitter",
@@ -263,6 +201,8 @@ require("lazy").setup({
         },
         {
             "folke/neodev.nvim",
+            ft = "lua",
+            opts = {},
             config = function()
                 require("neodev").setup({})
             end
